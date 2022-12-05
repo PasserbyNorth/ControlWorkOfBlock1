14e4a3705bb5e68932420604799a0e94c6e2fabe
PasserbyNorth <gitinishn@gmail.com>
2022-12-05 06:01:14 +0300

    add solution and finish modify README

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+
+Console.Clear();
+
+string[] arr = AskArray();
+
+string[] AskArray()
+{
+  Write("Введите значения через пробел: ");
+  return ReadLine().Split(" ");
+}
+
+string[] GetValidArray(string[] array)
+{
+  int size = array.Length;
+  int count = 0;
+  for (int i = 0; i < size; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      count++;
+    }
+  }
+  Console.WriteLine();
+
+
+  string[] ValidArray = new string[count];
+  {
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+      if (array[i].Length <= 3)
+      {
+        ValidArray[j++] = array[i];
+      }
+    }
+    return ValidArray;
+  }
+}
+string PrintArray(string[] array)
+{
+  int size = array.Length;
+  string print = String.Empty;
+  for (int i = 0; i < size; i++)
+  {
+    print += $"{array[i]} ";
+  }
+  return print;
+}
+
+Console.WriteLine();
+Console.WriteLine(PrintArray(arr));
+Console.WriteLine();
+string[] valid = (GetValidArray(arr));
+
+Console.WriteLine($"{PrintArray(valid)} ");
+Console.WriteLine();
